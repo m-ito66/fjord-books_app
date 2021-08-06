f318e94776308951181a5d63e9db46c62cb8bb63
m-ito <m-ito@ruby-dev.jp>
2021-08-06 15:51:33 +0900

    ユーザ一覧ページ、個別ページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'users/show'
   devise_for :users
   get 'users', to: 'users#index'
-  get 'users/:id', to: 'users#show'
+  get 'users/:id', to: 'users#show', as: 'user'
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
