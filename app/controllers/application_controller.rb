69768fb287bbe3f980db7f7b0e3ba1a55ccd3ccd
m-ito <m-ito@ruby-dev.jp>
2021-08-06 14:41:03 +0900

    ユーザー登録ページを修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:postal_code])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:address])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:self_introduction])
+  end
 end
