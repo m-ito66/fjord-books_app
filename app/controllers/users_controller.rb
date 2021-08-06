ef43579fdd004ce9ebd41a1b1911300ad552eaa9
m-ito <m-ito@ruby-dev.jp>
2021-08-07 00:04:16 +0900

    日本語対応（viewなど含めて）

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def index
-    @users = User.order(:id).page(params[:page])
+    @users = User.order(:id).page(params[:page]).per(3)
   end
 
   def show
