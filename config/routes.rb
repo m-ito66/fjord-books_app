84cf9571395058d225e2ba7249a53c12449c4150
m-ito <m-ito@ruby-dev.jp>
2021-08-06 14:53:05 +0900

    ルーティング設定の追加と、コントローラ作成（ユーザー一覧と個別ページ）

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+  get 'users', to: 'users#index'
+  get 'users/:id', to: 'users#show'
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
