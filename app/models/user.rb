8db55b6fd909f176a670fbc85509de7e36c726c1
m-ito <m-ito@ruby-dev.jp>
2021-08-08 17:56:45 +0900

    プロフィール画像のバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :avatar
+
+  validate :avatar_type
+
+  private
+
+  def avatar_type
+    return unless avatar.attached?
+    if !avatar.blob.content_type.in?(%('image/jpeg image/png image/gif'))
+      errors.add(:avatar, 'はjpegまたはpng形式またはgif形式でアップロードしてください')
+    end
+  end
 end
