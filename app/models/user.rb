7025c12630c67184083a0368159cf2bdcc54aa6d
m-ito <m-ito@ruby-dev.jp>
2021-08-09 13:24:05 +0900

    rubocop通す

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :avatar
-  has_many :active_relationships, class_name: "Relationship",
-            foreign_key: "follower_id", dependent: :destroy
-  has_many :passive_relationships, class_name: "Relationship",
-            foreign_key: "followed_id", dependent: :destroy
+  has_many :active_relationships, class_name: 'Relationship', inverse_of: :follower,
+                                  foreign_key: 'follower_id', dependent: :destroy
+  has_many :passive_relationships, class_name: 'Relationship', inverse_of: :followed,
+                                   foreign_key: 'followed_id', dependent: :destroy
   has_many :followings, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
 
