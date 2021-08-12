e1458ef1ed5a46b4e5bb8b8b085aa913e020a6ed
m-ito <m-ito@ruby-dev.jp>
2021-08-13 00:05:31 +0900

    rubocopの指摘を修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:postal_code, :address, :self_introduction])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:postal_code, :address, :self_introduction])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[postal_code address self_introduction])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[postal_code address self_introduction])
   end
 end
