fe95f6bdf61fc96b5c4d96ef26546081ea60f75b
m-ito <m-ito@ruby-dev.jp>
2021-08-14 18:28:23 +0900

    image?メソッドを使用

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,6 @@ class User < ApplicationRecord
   def avatar_type
     return unless avatar.attached?
 
-    errors.add(:avatar, 'はjpegまたはpng形式またはgif形式でアップロードしてください') unless avatar.blob.content_type.in?(%('image/jpeg image/png image/gif'))
+    errors.add(:avatar, 'はjpegまたはpng形式またはgif形式でアップロードしてください') unless avatar.image?
   end
 end
