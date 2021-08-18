1d0a3a7c8274818a8e040ca95a0f2b49dc61ad3a
m-ito <m-ito@ruby-dev.jp>
2021-08-18 10:59:28 +0900

    reportsモデルテストのsetupを削除

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,14 +3,11 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  def setup
-    @user = users(:alice)
-    @another = users(:bob)
-    @report = @user.reports.build(title: 'Hello', content: "I'm Alice.")
-  end
-
   test '#editable?' do
-    assert @report.editable?(@user)
-    assert_not @report.editable?(@another)
+    user = users(:alice)
+    another = users(:bob)
+    report = user.reports.build(title: 'Hello', content: "I'm Alice.")
+    assert report.editable?(user)
+    assert_not report.editable?(another)
   end
 end
