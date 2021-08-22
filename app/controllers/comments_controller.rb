dce4dac7db2f8d82d28e008fa3b482d9490e962b
m-ito <m-ito@ruby-dev.jp>
2021-08-22 15:23:38 +0900

    commentコントローラで、リダイレクトの修正、reportコントローラで、jsonファイル関係の記載を削除

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,26 +3,19 @@
 class CommentsController < ApplicationController
   before_action :set_commentable
   before_action :set_comment, only: [:destroy]
-  before_action :correct_user, only: [:destroy]
 
   # POST /reports/1/comments
   def create
     @comment = @commentable.comments.build(comment_params)
     @comment.user = current_user
-    if @comment.save
-      redirect_to @commentable, notice: t('controllers.common.notice_create', name: Comment.model_name.human)
-    else
-      rednder @commentable
-    end
+    @comment.save
+    redirect_to @commentable, notice: t('controllers.common.notice_create', name: Comment.model_name.human)
   end
 
   # DELETE /reports/1/comments/1
   def destroy
-    if @comment.destroy
-      redirect_to @commentable, notice: t('controllers.common.notice_destroy', name: Comment.model_name.human)
-    else
-      render @commentable
-    end
+    @comment.destroy
+    redirect_to @commentable, notice: t('controllers.common.notice_destroy', name: Comment.model_name.human)
   end
 
   private
@@ -32,15 +25,10 @@ class CommentsController < ApplicationController
   end
 
   def set_comment
-    @comment = Comment.find(params[:id])
+    @comment = current_user.comments.find(params[:id])
   end
 
   def comment_params
     params.require(:comment).permit(:content)
-  end
-
-  def correct_user
-    @comment = current_user.comments.find_by(id: params[:id])
-    redirect_to reports_url if @comment.nil?
   end
 end
